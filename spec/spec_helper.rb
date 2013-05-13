46f45ed5a4ae9003cc7e9b53244acfa85e98a03f
KalabiYau <skullzeek@gmail.com>
2013-05-13 22:20:48 +0400

    cleanup spec helper. Get rid of unused or ambiguous reqs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,39 +1,20 @@
-# Configure Rails Envinronment
-ENV["RAILS_ENV"] = "test"
-require 'ruby-debug'
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
-require "rspec/rails"
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../dummy/config/environment.rb',  __FILE__)
+require 'rspec/rails'
 
 ActionMailer::Base.delivery_method = :test
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.default_url_options[:host] = "test.com"
-
-Rails.backtrace_cleaner.remove_silencers!
-
-# Configure capybara for integration testing
-#require "capybara/rails"
-#Capybara.default_driver   = :rack_test
-#Capybara.default_selector = :css
-
-# Run any available migration
-ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
-
-# Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
-Rspec.configure do |config|
-  # Remove this line if you don't want Rspec's should and should_not
-  # methods or matchers
-  require 'rspec/expectations'
-  config.include Rspec::Matchers
-
-  config.fixture_path = File.join(File.dirname(__FILE__), "fixtures")
+RSpec.configure do |config|
+  config.fixture_path = File.join(File.dirname(__FILE__), 'fixtures')
   config.use_transactional_fixtures = true
-
-  # == Mock Framework
   config.mock_with :rspec
-
+  config.before(:suite) do
+    ActiveRecord::Schema.verbose = false
+    load 'dummy/db/schema.rb'
+  end
 end
 
 
