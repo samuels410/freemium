22363d91bf79c32b5b60e067a09a1c37812aa6dd
KalabiYau <skullzeek@gmail.com>
2013-05-13 22:40:16 +0400

    Move env related vars to dummy env file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,6 @@
 ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 require 'rspec/rails'
-
-ActionMailer::Base.delivery_method = :test
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.default_url_options[:host] = "test.com"
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
